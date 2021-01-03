18c7f960f7442fa77273d467186447212ddd0d39
jannes <jannes.dev@posteo.net>
2021-01-03 20:10:27 +0100

    Fix deadlock (hopefully)

--- rocksdb-6.7.3/util/threadpool_imp.cc
+++ rocksdb-6.7.3/util/threadpool_imp.cc
@@ -239,12 +239,11 @@ void ThreadPoolImpl::Impl::BGThread(size_t thread_id) {
 
     if (this->is_adaptive) {
       int to_scale = get_scaling_advice(convert_queue_len(queue_len_.load()));
-      if (this->total_threads_limit_ + to_scale > 0) {
-        this->total_threads_limit_ += to_scale;
+      int new_thread_limit = this->total_threads_limit_ + to_scale;
+      if (new_thread_limit < 1) {
+        new_thread_limit = 1;
       }
-      else {
-        this->total_threads_limit_ = 1;
-      }
+      this->SetBackgroundThreadsInternal(new_thread_limit, true);
     }
 
     if (exit_all_threads_) {  // mechanism to let BG threads exit safely
